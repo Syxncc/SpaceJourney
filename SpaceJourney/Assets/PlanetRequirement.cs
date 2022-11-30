2fc6fbfc3212b6a41e6ad565410d78babfd4e2d8
marjieee <marj8212@gmail.com>
2022-11-30 21:02:54 +0800

    Moving Camera and player in scene, collider trigger in space scene

--- SpaceJourney/Assets/PlanetRequirement.cs
+++ SpaceJourney/Assets/PlanetRequirement.cs
@@ -9,11 +9,19 @@ public class PlanetRequirement : MonoBehaviour
     public GameObject planet;
 
     public GameObject requirements;
+    public GameObject UI1;
+    public GameObject UI2;
+    public GameObject UI3;
+    public GameObject UI4;
 
     
     public void OnTriggerEnter(Collider other) {
         Debug.Log("may nangyare");
         requirements.SetActive(true);
+        UI1.SetActive(false);
+        UI2.SetActive(false);
+        UI3.SetActive(false);
+        UI4.SetActive(false);
     }
     
     // Start is called before the first frame update
