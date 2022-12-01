91bbfe2c2ea1bae6d61586ffe692ea4f7baacc99
Syxncc <109085280+Syxncc@users.noreply.github.com>
2022-12-02 01:50:41 +0800

    1:50am

--- SpaceJourney/Assets/PlanetRequirement.cs
+++ SpaceJourney/Assets/PlanetRequirement.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class PlanetRequirement : MonoBehaviour
 {
-    //public Gear gear;
+
     public GameObject spaceShip;
     public GameObject planet;
 
@@ -15,59 +15,42 @@ public class PlanetRequirement : MonoBehaviour
     public Button landButton;
     public GameObject loadingScene;
 
+    
     private bool requirementPassed;
 
-    
-    //public bool isPressed;
     private string planetName;
 
     public TextMeshProUGUI requirementText;
     
+
     void Start(){
         requirementPassed = false;
         landButton.enabled = false;
     }
 
-    void Update()
-    {
-        if(planetName!=null)
-        {
-           Debug.LogError(planetName); 
-        }
+    void Update(){
         
     }
-    
+
     public void OnCollisionEnter(Collision other) 
     {
         if (other.gameObject.CompareTag("Earth")){
             planetName = "Earth";
             requirements.SetActive(true);
-            if(requirementPassed)
-            {
+            if (requirementPassed){
                 requirementPassed = false;
             }
-            
         }
         else if (other.gameObject.CompareTag("Mars")){
             planetName = "Mars";
             requirements.SetActive(true);
-            if(requirementPassed)
-            {
+            if (requirementPassed){
                 requirementPassed = false;
             }
         }
-
+        
         requirement();
-
-    
     }
-
-    
-    // void OnTriggerExit(Collider other)
-    // {
-    //     requirements.SetActive(false);
-    //     requirementPassed = false;
-    // }
 
     // public void Earth()
     // {
@@ -105,67 +88,55 @@ public class PlanetRequirement : MonoBehaviour
 
     public void landing()
     {
-        if(requirementPassed)
-        {
-            if(planetName.Equals("Earth"))
-            {
+        if (requirementPassed){
+            if(planetName.Equals("Earth")){
                 loadingScene.SetActive(true);
                 // AsyncOperation operation = SceneManager.LoadSceneAsync(1);
                 SceneManager.LoadScene(1);
             }
-            else if(planetName.Equals("Mars"))
-            {
+            else if(planetName.Equals("Mars")){
                 loadingScene.SetActive(true);
                 // AsyncOperation operation = SceneManager.LoadSceneAsync(3);
                 SceneManager.LoadScene(3);
             }
-            else if(planetName.Equals("Mercury"))
-            {
+            else if(planetName.Equals("Mercury")){
                 loadingScene.SetActive(true);
                 // AsyncOperation operation = SceneManager.LoadSceneAsync(4);
                 SceneManager.LoadScene(4);
             }
-            else if(planetName.Equals("Venus"))
-            {
+            else if(planetName.Equals("Venus")){
                 loadingScene.SetActive(true);
                 // AsyncOperation operation = SceneManager.LoadSceneAsync(5);
                 SceneManager.LoadScene(5);
             }
-            else if(planetName.Equals("Asteroid Belt"))
-            {
+            else if(planetName.Equals("Asteroid Belt")){
                 loadingScene.SetActive(true);
                 // AsyncOperation operation = SceneManager.LoadSceneAsync(6);
                 SceneManager.LoadScene(6);
             }
-        }   
+        }
+        
     }
 
     public void requirement()
     {
         requirements.SetActive(true);
-        if(planetName.Equals("Earth"))
-        {
+        if(planetName.Equals("Earth")){
             requirementText.text = "Welcome Back! Have fun in your travel around the space";
             requirements.SetActive(true);
         }
-        else if(planetName.Equals("Mars"))
-        {
-            
-            if(ShopManager.walkLevel >= 4 && ShopManager.sprintLevel >= 4 )
-            {
+        else if(planetName.Equals("Mars")){
+            if (ShopManager.walkLevel >= 4 && ShopManager.sprintLevel >= 4){
                 requirementText.text = "Player Requirement\n\n Walk - lvl.4\n Sprint - lvl.4\n Earth InfoCard";
                 requirementPassed = true;
                 landButton.enabled = true;
-                Debug.LogError("Landed");
+             
             }
-            else
-            {
-                requirementText.text = "Already Fulfilled!\nPrepare to land in the land of Mars";
+            else{
+                requirementText.text = "Player Requirement\n\n Walk - lvl.4\n Sprint - lvl.4\n Earth InfoCard";
                 landButton.enabled = false;
-                
             }
-            
-            
+        
         }
         else if(planetName.Equals("Mercury"))
         {
@@ -182,6 +153,8 @@ public class PlanetRequirement : MonoBehaviour
             requirementText.text = "Spaceship Requirement\n\n Speed - lvl.3\n Boost - lvl.3\n Earth InfoCard\nMars InfoCard\nMercury InfoCard\nVenus InfoCard";
             requirements.SetActive(true);
         }
+
+        
         // else if (planetName.Equals("Jupiter"))
         // {
             
@@ -200,6 +173,5 @@ public class PlanetRequirement : MonoBehaviour
         // }
 
     }
-    
-    
+
 }
---
+++ SpaceJourney/Assets/Scripts/Planet/PlanetStats.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetStats : MonoBehaviour
+{
+    // public string planetName;
+
+    // public GameObject requirements;
+    // public Button landButton;
+    // public GameObject loadingScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // public void OnCollisionEnter(Collision other) 
+    // {
+    //     if (other.gameObject.CompareTag("Player")){
+    //        Debug.LogError("Wieeee");
+    //     }
+    // }
+
+    public void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Player"){
+           Debug.LogError("Wieeee");
+        }
+    }
+        
+        
+
+
+}
